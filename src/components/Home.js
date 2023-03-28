c6271bc85abc79397641bac87f8ffb6aa4732695
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2023-03-28 13:43:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-2xl sm:text-6xl font-bold text-white">
-            I'm a ReactJS Developer
+            I'm a React Developer
           </h2>
 
           <p className="text-gray-400 py-4 max-w-md">
